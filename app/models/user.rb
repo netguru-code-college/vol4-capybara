b543347c34da39cbbc672039bd6c789da9f8dd60
zaneta uba <zaneta.uba@gmail.com>
2019-02-18 18:12:26 +0200

    work in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    mount_uploader :avatar, AvatarUploader
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
