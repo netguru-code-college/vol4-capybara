063ab9941ce2c366fde40b102c5ceba0f6d825d8
Kaja <kajka.c@gmail.com>
2019-02-18 16:47:15 +0100

    sign in by facebook is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
