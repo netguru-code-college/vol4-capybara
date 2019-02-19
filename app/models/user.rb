57bce169893ae830e3bcd156f8e6661b8e592167
Kaja <kajka.c@gmail.com>
2019-02-19 13:27:15 +0100

    added all link with facebook functionalities

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,17 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook]
          
     def self.from_omniauth(auth)
-          where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-            user.email = auth.info.email
-            user.provider = auth.provider
-            user.uid = auth.uid
-            user.password = Devise.friendly_token[0,20]
-        end
+      oauth_user = where(provider: auth.provider, uid: auth.uid).first
+
+      return oauth_user if oauth_user
+
+      find_or_initialize_by(email: auth.info.email).tap do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.password = Devise.friendly_token[0,20] if user.new_record?
+
+        user.save!
+      end
     end
     
 end
